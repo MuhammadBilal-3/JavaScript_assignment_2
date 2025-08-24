3334822ab0e2d4381758027c6d7c86989b6cdd79
Muhammad Bilal <muhammadbilal23031@gmail.com>
2025-08-24 20:05:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+     // Name: Muhammad Bilal
+    // Date: 6 - 8 - 2025
+    // Batch: 17
+
+
+// 1. Create a full name message using variables.Combine first and last name into fullName and alert
+// it.
+
+// let a = "Muhammada";
+// let b = "Bilal";
+// let c = a + b;
+// console.log(c);
+
+
+// 2. What will be the output of:
+// let a = "5";
+// let b = 2;
+// let result = a + b;
+// console.log(result);
+//  Explain why.
+
+
+
+
+
+// 3. Guess the output and explain:
+// let a = 10;
+// let b = 5;
+// let c = ++a + b-- + --b;

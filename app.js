7ec35cc6da28ae59807795ac8c02eccdf4f14514
Muhammad Bilal <muhammadbilal23031@gmail.com>
2025-08-24 20:10:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,127 @@
+// NAME: MUHAMMAD BILAL.
+// FATHER NAME: SHAMS UD DIN.
+// BATCH: 14.
+// CAMPUS: NORTH KARACHI.
+
+
+
+
+// Q 1.
+
+// var name = prompt("what is your name?");
+// alert ("hello, " + " " + "! welcome to javascript!")
+
+
+// Q 2.
+
+// let a = prompt("Enter a Number");
+// if (a > 10) {
+//     "number is big"
+//     alert("Number is big");
+// }
+
+
+// Q 3.
+
+// var sum = (50 + 25);
+// alert(sum);
+
+
+// Q 4.
+
+// let a = prompt("Enter your favorite color");
+// alert("wow! [color] is my favorite too!");
+
+
+// Q 5.
+
+// let ask_age = prompt ("age");
+// if ( ask_age >= 18) {
+// alert("you are eligible to vote");  
+// }
+
+
+// Q 6.
+
+// let a = prompt("enter a number");
+// let b = prompt("enter a second number to sum");
+// alert(a + b);
+
+
+// Q 7.
+
+// let ask = prompt("do you like coffee?");
+// if (ask >= "yes") {
+//     alert ("Great");
+// }
+
+
+// Q 8.
+
+// let userPassword = prompt("inter your password");
+// if (userPassword == 12345) {
+// alert("Access Grantes");    
+// }
+
+
+// Q 9.
+
+// let a = prompt("enter a number");
+// let b = prompt("confirm number");
+// if (a === b) {
+//     alert("both are equal");
+// }
+
+
+// Q 10;
+
+// let myName = prompt("enter a Name");
+// if (myName === "Bilal") {
+//     alert("Thanks! Bilal");
+// }
+
+
+// Q 11.
+
+// let temperature = prompt("whai is the temperature today");
+// let temperatureToDay = (30);
+// if (temperature >= temperatureToDay) {
+//  alert("Its's hot today"); 
+// }
+
+
+// Q 12.
+
+// let a = ("Hello");
+// let b = ("World");
+// alert(a + " " + b);
+
+
+// Q 13.
+
+// let num = prompt("Enter a Number");
+// num = Number(num);
+// if (num % 2 === 0) {
+//     alert("Even Number");
+// } else {
+//     alert("Odd Number");
+// }
+
+
+// Q 14.
+
+// let city = prompt("your city name?");
+// if (city == "Karachi") {
+//     alert(CityOfLight = "Welcome To The City Of Light");
+// }
+
+
+// Q 15.
+
+// let marks = prompt("What's Your Marks?");
+// let passingMarks = ("50");
+// if (marks >= passingMarks) {
+//     alert("Pass");
+// } else {
+//     alert("Fail");
+// }
